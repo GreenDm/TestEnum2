9c27870a87e6a79c59c32dca7703d10f846dad57
HrinchenkoDmytro <D.greenchenko@gmail.com>
2016-03-11 20:57:40 +0200

    Update Main.java

--- src/com/epam/Main.java
+++ src/com/epam/Main.java
@@ -10,30 +10,11 @@ public class Main {
 
         for (MyDayOfWeek dc : MyDayOfWeek.values()) {
             switch (dc) {
-                case SUNDAY:
-                    System.out.println("My Java day: " + dc);
-break;
                 case MONDAY:
-                    System.out.println("My Java day: " + dc);
-                    break;
-                case TUESDAY:
-                    System.out.println("My Java day: " + dc);
-                    break;
                 case WEDNESDAY:
-                    System.out.println("My Java day: " + dc);
-                    break;
-                case THURSDAY:
-                    System.out.println("My Java day: " + dc);
-                    break;
                 case FRIDAY:
                     System.out.println("My Java day: " + dc);
-                    break;
-                case SATURDAY:
-                    System.out.println("My Java day: " + dc);
-                    break;
-                default:
-                    System.out.println("Invali name of day");
-
+                   
             }
         }
     }
